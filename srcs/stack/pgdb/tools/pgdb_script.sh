a7b1e6c96247b6fca68cfbd505fb5d6900c10781
cynthia glardon <cynthia.glardon@formation.orif.ch>
2026-09-23 16:24:12 +0200

    paths

--- srcs/stack/pgdb/tools/pgdb_script.sh
+++ srcs/stack/pgdb/tools/pgdb_script.sh
@@ -2,3 +2,16 @@
 
 sudo systemctl start postgresql
 sudo systemctl status postgresql
+
+echo "==== [PGDB] Readying configuration file ===="
+PGSQL_USER=$(grep "^PGSQL_USER" /run/secrets/DB_credentials | sed 's/PGSQL_USER=//')
+PGSQL_PASSWORD=$(grep "^PGSQL_PASSWORD" /run/secrets/DB_credentials | sed 's/PGSQL_PASSWORD=//')
+PGSQL_ROOT_PASSWORD=$(grep "^PGSQL_ROOT_PASSWORD" /run/secrets/DB_credentials | sed 's/PGSQL_ROOT_PASSWORD=//')
+
+docker run \
+	-e POSTGRES_PASSWORD=mypassword \
+	-e POSTGRES_USER=myuser \
+	-e POSTGRES_DB=mydatabase \
+	(-p 5432:5432 \
+	-v postgres-data:/var/lib/postgresql/data \
+	-d postgres)
